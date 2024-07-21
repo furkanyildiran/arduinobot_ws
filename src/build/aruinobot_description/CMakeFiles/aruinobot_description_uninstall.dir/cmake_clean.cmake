22d37d2b3d88c35300e1e56dc155890d701da933
furkanyildiran <furkanyldrn0@gmail.com>
2024-07-21 15:26:39 +0300

    first commit

---
+++ src/build/aruinobot_description/CMakeFiles/aruinobot_description_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aruinobot_description_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/aruinobot_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
